17344fa852424e91d31fec0bd447c67560ff7900
M <M>
2025-01-26 14:14:34 +0200

    ++

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,12 +2,10 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-        if (from1UpTo1000 >= 1 && from1UpTo1000 <= 1000) {
-                if (from1UpTo1000 < 500) {
-                    System.out.println("Eagle");
-                } else {
-                    System.out.println("Tail");
-                }
+        if (from1UpTo1000 < 500) {
+            System.out.println("Eagle"); // Виводимо "Eagle", якщо число менше 500
+        } else {
+            System.out.println("Tail"); // Виводимо "Tail", якщо число 500 або більше
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,7 +2,10 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        boolean max = first > second;
-        System.out.println(max);
+        if (first > second) {
+            System.out.println(first); // Виводимо перше число, якщо воно більше
+        } else {
+            System.out.println(second); // Виводимо друге число, якщо воно більше або рівне
+        }
     }
 }
